d91c310a1ce864b84fce3863b4135bb5d7c27ff9
James Donitzen <James.Donitzen2@gmail.com>
2019-07-18 18:52:30 -0500

    Adding JSONDotNet library for json messaging - ceated script to send json to localhost:8095

---
+++ Assets/Scripts/LocalServerNetwork.cs
@@ -0,0 +1,67 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+using System.Net;
+using UnityEngine;
+
+public class LocalServerNetwork : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    public void ServerTest()
+    {
+        JsonSerializer serializer = new JsonSerializer();
+        GameEvent userAction = new GameEvent();
+        userAction.setAction("TEST");
+
+        string jsonString = JsonConvert.SerializeObject(userAction);
+
+        /* could also use
+              string json = new JavaScriptSerializer().Serialize(new
+                {
+                    user = "Foo",
+                    password = "Baz"
+                });
+         */
+
+        SendJSON(jsonString);
+        
+        return;
+    }
+
+    public void SendJSON( string jsonString)
+    {
+        HttpWebRequest jsonReq = (HttpWebRequest)WebRequest.Create("http://localhost:8095/");
+        jsonReq.ContentType = "application/json";
+        jsonReq.Method = "POST";
+
+        using (StreamWriter streamWriter = new StreamWriter(jsonReq.GetRequestStream()))
+        {
+            streamWriter.Write(jsonString);
+        }
+
+        HttpWebResponse jsonResp = (HttpWebResponse)jsonReq.GetResponse();
+        return;
+    }
+}
+
+public class GameEvent
+{
+    public string action;
+
+    public GameEvent()
+        {
+        }
+
+    public void setAction(string action)
+    {
+        this.action = action;
+    }
+
+    public string getAction()
+    {
+        return this.action;
+    }
+}
