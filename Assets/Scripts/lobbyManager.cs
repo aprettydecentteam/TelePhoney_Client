f0ec168a41abe4e92494e40372434da00e7f6676
Chris Bushman <christopherwbushman@gmail.com>
2019-07-29 15:28:07 -0500

    Reworked client network code as static methods

--- Assets/Scripts/lobbyManager.cs
+++ Assets/Scripts/lobbyManager.cs
@@ -7,12 +7,32 @@ public class lobbyManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        NetworkMessaging.ConnectWebSocketToServerAsync();
+        Debug.Log("Opening socket connection");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (NetworkMessaging.socketOpen())
+        {
+            Debug.Log("Socket is open. Checking for messages...");
+            checkForMessage();
+        }
+    }
+
+    public void testSocketMessage() 
+    {
+        NetworkMessaging.SendSocketMessage("This is a test message");
+    }
+
+    private async void checkForMessage() 
+    {
+        string message = await NetworkMessaging.CheckSocketMessage();
+
+        if ( message != null && message != "" )
+        {
+            Debug.Log("Message was: " + message);
+        }
     }
 }
